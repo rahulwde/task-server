8581a3d6da66a8d68d17c67f5fbe831d615f8d03
rahul <rahulmondol6984@hmail.com>
2025-05-21 09:04:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const port = process.env.PORT || 5000
+app.use(cors())
+app.use(express.json())
+app.get("/",(req,res)=>{
+  res.send("coffee is hotter")
+})
+app.listen(port, ()=>{
+  console.log("coffe is server")
+})
