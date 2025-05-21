9df17cdf7f40521356fe13e31fe68597a9d5ed39
rahul <rahulmondol6984@hmail.com>
2025-05-21 18:08:34 +0600

    browsing

--- index.js
+++ index.js
@@ -37,6 +37,10 @@ async function run() {
       const result = await tasksCollection.find().limit(6).toArray()
       res.send(result)
     })
+    app.get('/tasks', async (req, res) => {
+  const result = await tasksCollection.find().toArray();
+  res.send(result);
+});
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
